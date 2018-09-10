5a72e5192d5ce0e5a8bd6b3db4ee126eca3839d5
pfittante1001 <pfittante@yahoo.com>
2018-09-10 11:21:05 -0400

    initial commit

---
+++ StringsHomework/StringsHomework.cs
@@ -0,0 +1,180 @@
+﻿namespace StringsHomework
+{
+    //Pete Fittante 09/06/2018
+    [TestFixture]
+    public class StringsHomework
+    {
+        private object __FILL_ME_IN__ = null;
+
+        //[Test]
+        //public void Concatenation_With_Plus_Operator()
+        //{
+        //    var one = "Hello";
+        //    var two = "World";
+
+        //    var allTogetherNow = one ? " " ? two ? "!";
+
+        //    Assert.That(allTogetherNow, Is.EqualTo(__FILL_ME_IN__));
+        //}
+
+        //[Test]
+        //public void Interpolation_With_StringFormat()
+        //{
+        //    var one = "Hello";
+        //    var two = "World";
+
+        //    // string.Format lets you do string interpolation, just like Console.Writeline, but
+        //    // it returns a string rather than printing one out to the console.
+        //    var allTogetherNow = string.Format("? ?!", one, two);
+
+        //    Assert.That(allTogetherNow, Is.EqualTo(__FILL_ME_IN__));
+        //}
+
+        //[Test]
+        //public void Equality_With_DoubleEquals_Operator()
+        //{
+        //    var one = "hello";
+        //    var two = "hello";
+
+        //    var result = one ? two;
+
+        //    Assert.That(result, Is.EqualTo(__FILL_ME_IN__));
+        //}
+
+        //[Test]
+        //public void Equality_With_DoubleEquals_Is_CaseSensitive()
+        //{
+        //    var one = "hello";
+        //    var two = "HELLO";
+
+        //    var result = one ? two;
+
+        //    Assert.That(result, Is.EqualTo(__FILL_ME_IN__));
+        //}
+
+        //[Test]
+        //public void Equality_With_Equals_Method()
+        //{
+        //    var one = "hello";
+        //    var two = "hello";
+
+        //    // You can also check equality using a method on a string
+        //    // instance.  It works exactly the same.
+        //    var result ? one.Equals(two);
+
+        //    Assert.That(result, Is.EqualTo(__FILL_ME_IN__));
+        //}
+
+        //[Test]
+        //public void Equality_With_Equals_Method_And_Arguments()
+        //{
+        //    var one = "hello";
+        //    var two = "HELLO";
+
+        //    // Why use the Equals method?
+        //    // Using the Equals method, you can use different overloads to get
+        //    // slightly different behavior (like ignoring case!).
+        //    var comparisonType = StringComparison.CurrentCultureIgnoreCase;
+        //    var result = one.Equals(?, ?);
+
+        //    Assert.That(result, Is.EqualTo(__FILL_ME_IN__));
+        //}
+
+        //[Test]
+        //public void Changing_String_Case()
+        //{
+        //    var mixedCase = "Los Angeles";
+
+        //    var lowerCase = mixedCase.?;
+        //    var upperCase = mixedCase.?;
+
+        //    Assert.That(mixedCase, Is.EqualTo(__FILL_ME_IN__));
+        //    Assert.That(lowerCase, Is.EqualTo(__FILL_ME_IN__));
+        //    Assert.That(upperCase, Is.EqualTo(__FILL_ME_IN__));
+        //}
+
+        //[Test]
+        //public void Length_Of_Strings()
+        //{
+        //    var myString = "Los Angeles";
+
+        //    var length = myString.?;
+
+        //    Assert.That(?, Is.EqualTo(__FILL_ME_IN__));
+        //}
+
+        //[Test]
+        //public void Escape_Characters_With_Strings()
+        //{
+        //    var myString = "\"hi\"";
+
+        //    Assert.That(myString.?, Is.EqualTo(__FILL_ME_IN__));
+        //    Assert.That(myString[?], Is.EqualTo(__FILL_ME_IN__));
+        //    Assert.That(myString[?], Is.EqualTo(__FILL_ME_IN__));
+        //    Assert.That(myString[?], Is.EqualTo(__FILL_ME_IN__));
+        //    Assert.That(myString[?], Is.EqualTo(__FILL_ME_IN__));
+        //}
+
+        //[Test]
+        //public void Trim_Removes_Whitespace_From_Beginning_And_End()
+        //{
+        //    var myString = "           hello    ";
+
+        //    var result = myString.?;
+
+        //    Assert.That(?, Is.EqualTo(__FILL_ME_IN__));
+        //}
+
+        //[Test]
+        //public void IndexOf_Returns_IndexOf_The_First_Character()
+        //{
+        //    var myString = "abcd";
+
+        //    Assert.That(myString.?('b'), Is.EqualTo(__FILL_ME_IN__));
+        //    Assert.That(myString.?("cd"), Is.EqualTo(__FILL_ME_IN__));
+        //}
+
+        //[Test]
+        //public void Contains_Returns_A_Boolean()
+        //{
+        //    var alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        //    Assert.That(alphabet.?("6"), Is.EqualTo(__FILL_ME_IN__));
+        //    Assert.That(alphabet.?("f"), Is.EqualTo(__FILL_ME_IN__));
+        //    Assert.That(alphabet.?("F"), Is.EqualTo(__FILL_ME_IN__));
+        //}
+
+        //[Test]
+        //public void StartsWith_Returns_Whether_A_String_StartsWith_Another_String()
+        //{
+        //    var alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        //    Assert.That(alphabet.?("abc"), Is.EqualTo(__FILL_ME_IN__));
+        //    Assert.That(alphabet.?("ABC"), Is.EqualTo(__FILL_ME_IN__));
+        //    Assert.That(alphabet.?("def"), Is.EqualTo(__FILL_ME_IN__));
+        //}
+
+        //[Test]
+        //public void EndsWith_Returns_Whether_A_String_EndsWith_Another_String()
+        //{
+        //    var alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        //    Assert.That(alphabet.?("abc"), Is.EqualTo(__FILL_ME_IN__));
+        //    Assert.That(alphabet.?("def"), Is.EqualTo(__FILL_ME_IN__));
+        //    Assert.That(alphabet.?("DEF"), Is.EqualTo(__FILL_ME_IN__));
+        //}
+
+        //[Test]
+        //public void Substring_Lets_You_Take_A_Slice_From_A_String()
+        //{
+        //    var alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        //    var startIndex = 2;
+        //    var length = 4;
+        //    var result = alphabet.?(startIndex, length);
+
+        //    Assert.That(?, Is.EqualTo(__FILL_ME_IN__));
+        //}
+    }
+}
+
